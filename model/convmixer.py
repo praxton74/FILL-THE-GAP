bb2576e8190f8273ec88d1c3c57c7c91952057ba
Parth <94388365+praxton74@users.noreply.github.com>
2023-11-23 18:54:33 +0530

    Update convmixer.py

--- model/convmixer.py
+++ model/convmixer.py
@@ -5,9 +5,6 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 import numpy as np
 
-"""
-Major implementations taken from: https://keras.io/examples/vision/convmixer/
-"""
 
 def activation_block(x):
     x = layers.Activation("gelu")(x)
@@ -20,32 +17,20 @@ def conv_stem(x, filters: int, patch_size: int):
 
 
 def conv_mixer_block(x, filters: int, kernel_size: int):
-    # Depthwise convolution.
     x0 = x
     x = layers.DepthwiseConv2D(kernel_size=kernel_size, padding="same")(x)
     x = layers.Add()([activation_block(x), x0])  # Residual.
-
-    # Pointwise convolution.
     x = layers.Conv2D(filters, kernel_size=1)(x)
-
     x = activation_block(x)
 
     return x
 
 
 def get_conv_mixer_256_8(image_size=32, filters=256, depth=2, kernel_size=5, patch_size=2):
-    """ConvMixer-256/8: https://openreview.net/pdf?id=TVHS5Y4dNvM.
-    The hyperparameter values are taken from the paper.
-    """
     inputs = keras.Input((image_size, image_size, 3))
-
-    # Extract patch embeddings.
     x = conv_stem(inputs, filters, patch_size)
-
-    # ConvMixer blocks.
     for i in range(depth):
         x = conv_mixer_block(x, filters, kernel_size)
 
     outputs = layers.AveragePooling2D((2, 2))(x) 
-
     return keras.Model(inputs, outputs)
